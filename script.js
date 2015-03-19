61273896d2e8d4b457dee65ebb66f237ad1aed22
Cliff Eby <ebyc@pbworld.com>
2015-03-18 22:17:02 -0400

    Initial commit

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // vim:set sw=2 sts=2 et:
 var canvas, ctx;
 
-function dist(px, py, pt) {
-  var dx = pt.x-px, dy = pt.y-py, dxx = dx*dx, dyy = dy*dy, dd = dxx+dyy, d = Math.sqrt(dd);
-  return d;
+//function dist(px, py, pt) {
+//  var dx = pt.x-px, dy = pt.y-py, dxx = dx*dx, dyy = dy*dy, dd = dxx+dyy, d = Math.sqrt(dd);
+//  return d;
+//}
+//function in_circle(px, py, circ) {
+//  console.log(px, py, circ);
+//  return dist(px, py, circ) < circ.r;
+//}
+//setInterval(function () {alert("Hello1")}, 3000);
+var min = 0;
+var maxX = width = 1100;
+var maxY = height = 400;
+function randomIntFromIntervalX(min,maxX)
+{
+    return Math.floor(Math.random()*(maxX-min+1)+min);
 }
-function in_circle(px, py, circ) {
-  console.log(px, py, circ);
-  return dist(px, py, circ) < circ.r;
+function randomIntFromIntervalX(min,maxY)
+{
+    return Math.floor(Math.random()*(maxY-min+1)+min);
 }
 
 function Canvas() {
@@ -17,86 +29,97 @@ function Canvas() {
   var height = canvas.height = canvas.clientHeight;
   var ctx = self.ctx = canvas.getContext('2d');
   var vertices = self.vertices = [];
-  var HANDLERADIUS = 4;
-  self.selectedVertex = null;
+ // var HANDLERADIUS = 4;
+ // self.selectedVertex = null;
+    var x = randomIntFromIntervalX(min,maxX);
+    var y = randomIntFromIntervalX(min,maxY);
+    console.log(x, 'y',y);
   self.drawCircle = function (x,y,r,c) {
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'red';
     ctx.strokeStyle = c || 'black';
     ctx.strokeWidth = 3;
     ctx.beginPath();
-    ctx.arc(x,y,r,0,Math.PI*2,true);
+    ctx.arc(x,y,40,0,Math.PI*2,true);
     ctx.fill();
     ctx.stroke();
   };
   self.clear = function () {
     ctx.clearRect(0,0,width,height);
   };
-  self.redraw = function () {
-    self.clear();
-    for (var i = 0, l = vertices.length; i < l; ++i) {
-      var v = vertices[i];
-      if (self.selectedVertex == i) {
-        self.drawCircle(v.x, v.y, v.r, 'red');
-        self.drawCircle(v.x, v.y, HANDLERADIUS, 'blue');
-        self.drawCircle(v.x+v.r, v.y, HANDLERADIUS, 'blue');
-      } else {
-        self.drawCircle(v.x, v.y, v.r);
-      }
-    }
-  };
-  self.addVertex = function (x, y, r) {
-    self.vertices.push({x:x,y:y,r:r});
-    self.redraw();
-  };
-  self.selectVertexAt = function (x, y) {
-    self.selectedVertex = null;
-    for (var i = 0, l = vertices.length; i < l; ++i) {
-      var v = vertices[i];
-      var dx = v.x-x, dy = v.y-y,
-          dxx = dx*dx, dyy = dy*dy,
-          rr = v.r*v.r;
-      if (dxx > rr || dyy > rr) continue;
-      var d = dxx+dyy;
-      if (d > rr) continue;
-      self.selectedVertex = i;
-      break;
-    }
-    self.redraw();
-  };
-  self.helloWorld = function () {
-    self.addVertex(50,50,10);
-    self.selectVertexAt(45,50);
-  };
-  var fake = {fake:1};
-  self.getSelectedVertex = function () {
-    if (self.selectedVertex == null) return fake;
-    return self.vertices[self.selectedVertex];
-  };
-  self.moveTo = function (x,y) {
-    var v = self.getSelectedVertex();
-    v.x = x;
-    v.y = y;
-    self.redraw();
-  };
-  self.resizeTo = function (x,y) {
-    var v = self.getSelectedVertex();
-    v.r = dist(x, y, v);
-    self.redraw();
-  };
-  self.moveHandle = function (x, y) {
-    var v = self.getSelectedVertex();
-    if (v.fake) return false;
-    return in_circle(x, y, {x:v.x, y:v.y, r:HANDLERADIUS});
-  };
-  self.resizeHandle = function (x, y) {
-    var v = self.getSelectedVertex();
-    if (v.fake) return false;
-    return in_circle(x, y, {x:v.x+v.r, y:v.y, r:HANDLERADIUS});
-  };
+  //self.redraw = function () {
+  //  self.clear();
+  //  for (var i = 0, l = vertices.length; i < l; ++i) {
+  //    var v = vertices[i];
+  //    if (self.selectedVertex == i) {
+  //      self.drawCircle(v.x, v.y, 40, 'red');
+  //      //self.drawCircle(v.x, v.y, HANDLERADIUS, 'blue');
+  //      //self.drawCircle(v.x+v.r, v.y, HANDLERADIUS, 'blue');
+  //    } else {
+  //      self.drawCircle(x, y, v.r);
+  //    }
+  //  }
+  //};
+  //self.addVertex = function (x, y, r) {
+  //  self.vertices.push({x:x,y:y,r:r});
+  //  self.redraw();
+  //};
+  //self.selectVertexAt = function (x, y) {
+  //  self.selectedVertex = null;
+  //  for (var i = 0, l = vertices.length; i < l; ++i) {
+  //    var v = vertices[i];
+  //    var dx = v.x-x, dy = v.y-y,
+  //        dxx = dx*dx, dyy = dy*dy,
+  //        rr = v.r*v.r;
+  //    if (dxx > rr || dyy > rr) continue;
+  //    var d = dxx+dyy;
+  //    if (d > rr) continue;
+  //    self.selectedVertex = i;
+  //    break;
+  //  }
+  //  self.redraw();
+  //};
+  //self.helloWorld = function () {
+  //  self.addVertex(50,50,10);
+  //  self.selectVertexAt(45,50);
+  //};
+  //var fake = {fake:1};
+  //self.getSelectedVertex = function () {
+  //  if (self.selectedVertex == null) return fake;
+  //  return self.vertices[self.selectedVertex];
+  //};
+  //self.moveTo = function (x,y) {
+  //  var v = self.getSelectedVertex();
+  //  v.x = x;
+  //  v.y = y;
+  //  self.redraw();
+  //};
+//  self.resizeTo = function (x,y) {
+//    var v = self.getSelectedVertex();
+//    v.r = dist(x, y, v);
+//    self.redraw();
+//  };
+//  self.moveHandle = function (x, y) {
+//    var v = self.getSelectedVertex();
+//    if (v.fake) return false;
+//    return in_circle(x, y, {x:v.x, y:v.y, r:HANDLERADIUS});
+//  };
+//  self.resizeHandle = function (x, y) {
+//    var v = self.getSelectedVertex();
+//    if (v.fake) return false;
+//    return in_circle(x, y, {x:v.x+v.r, y:v.y, r:HANDLERADIUS});
+//  };
 }
 function init() {
   canvas = new Canvas();
-  canvas.helloWorld();
+        setInterval(function () {
+            canvas.clearRect(0,0,width,height);
+            for (var i = 0, l = 1; i < l; ++i) {
+                var x = randomIntFromIntervalX(min, maxX);
+                var y = randomIntFromIntervalX(min, maxY);
+                canvas.drawCircle(x, y, 40, 'red');
+                console.log(x, 'y', y);}
+        }, 3000);
+
 
   function mousemove_moving(ev) {
     if (!ev) ev = window.event;
