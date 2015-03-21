f9c8ef8db9adb32380bc49ef420b6304510a3201
cliffeby <clifford.eby@gmail.com>
2015-03-21 00:34:33 -0400

    working circles on canvas with click to clear

--- script.js
+++ script.js
@@ -1,79 +1,75 @@
-// vim:set sw=2 sts=2 et:
 var canvas, ctx;
-
-
-//setInterval(function () {alert("Hello1")}, 3000);
 var min = 0;
 var maxX = width = 1100;
 var maxY = height = 400;
-function randomIntFromIntervalX(min,maxX)
-{
-    return Math.floor(Math.random()*(maxX-min+1)+min);
+var beginTime;
+var endTime;
+var correct =[,,];
+var incorrect = [,,];
+
+function randomIntFromIntervalX(min, width) {
+    return Math.floor(Math.random() * (width - min + 1) + min);
 }
-function randomIntFromIntervalX(min,maxY)
-{
-    return Math.floor(Math.random()*(maxY-min+1)+min);
+function randomIntFromIntervalX(min, height) {
+    return Math.floor(Math.random() * (height - min + 1) + min);
 }
 
 function Canvas() {
     var self = this;
     var canvas = self.canvas = document.getElementById('thecanvas');
-    canvas.addEventListener('click', function() { }, false);
+    canvas.addEventListener('click', function () {
+    }, false);
     var width = canvas.width = canvas.clientWidth;
     var height = canvas.height = canvas.clientHeight;
     var ctx = self.ctx = canvas.getContext('2d');
-    var vertices = self.vertices = [];
-    // var HANDLERADIUS = 4;
 
-
-    self.drawCircle = function (x,y,r,c) {
+    self.drawCircle = function (x, y, r, c) {
         ctx.fillStyle = 'red';
         ctx.strokeStyle = c || 'black';
         ctx.strokeWidth = 3;
         ctx.beginPath();
-        ctx.arc(x,y,40,0,Math.PI*2,true);
+        ctx.arc(x, y, 40, 0, Math.PI * 2, true);
         ctx.fill();
         ctx.stroke();
     };
     self.clear = function () {
-        ctx.clearRect(0,0,width,height);
+        ctx.clearRect(0, 0, width, height);
     };
     var dotLeft = canvas.offsetLeft;
     var dotTop = canvas.offsetTop;
 
-    canvas.addEventListener('click', function(event){
+    canvas.addEventListener('click', function (event) {
         var x = event.pageX - dotLeft;
         var y = event.pageY - dotTop;
-        console.log('x = ',x, 'y = ',y, dotY);
-        if (y > dotY-10 && y < dotY+10
-            && x > dotX-10 && x < dotX+10) {
-           // alert('clicked an element');
+        console.log('x = ', x, 'y = ', y, dotY);
+        if (y > dotY - 10 && y < dotY + 10
+            && x > dotX - 10 && x < dotX + 10) {
+            endTime = new Date();
             clearInterval(myTimer);
-        init();
-            //myTimer = setInterval(drawMyDot, myTimer);
+            correct.push(x,y,endTime-beginTime);
+            console.log('correct',correct);
+            init();
+        }else{
+            incorrect.push(x,y,endTime-beginTime);
+            console.log('incorrect',incorrect);
         }
+
     })
-
 }
-
-function init() {
-     canvas = new Canvas();
+function placeDot() {
     canvas.clear();
-    dotX = randomIntFromIntervalX(20, maxX-100);
-    dotY = randomIntFromIntervalX(20, maxY+200);
+    dotX = randomIntFromIntervalX(20, maxX - 100);
+    dotY = randomIntFromIntervalX(20, maxY + 200);
     canvas.drawCircle(dotX, dotY, 40, 'red');
+    beginTime = new Date();
     console.log(dotX, 'y', dotY);
-     myTimer = setInterval(drawMyDot = function () {
-            canvas.clear();
-            dotX = randomIntFromIntervalX(20, maxX-100);
-            dotY = randomIntFromIntervalX(20, maxY+200);
-            canvas.drawCircle(dotX, dotY, 40, 'red');
-            console.log(dotX, 'y', dotY);
+};
+function init() {
+    canvas = new Canvas();
+    placeDot();
+    myTimer = setInterval(drawMyDot = function () {
+            placeDot();
         },
         5000);
 };
-
-for (var i = 0;i<10;i++) {
-    window.onload = init;
-    //canvas.clear();
-}
+window.onload = init;
